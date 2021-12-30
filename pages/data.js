b91dd7aaba02f07458f2bc7cf1e5b2e9cf83901d
iblurdesigner <nevomen@gmail.com>
2021-12-30 12:58:32 -0500

    Primer commit con nueva version de  Next 12

---
+++ pages/data.js
@@ -0,0 +1,75 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "CEIQ Surgical Skin 1.0",
+      category: "skins",
+      image: "/images/skin1.jpg",
+      price: 15,
+      brand: "CEIQ",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Piel sintética docente",
+    },
+    {
+      _id: "2",
+      name: "Juego Lap Aro",
+      category: "endo training",
+      image: "/images/juegoLap.jpg",
+      price: 5,
+      brand: "CEIQ",
+      rating: 4.0,
+      numReviews: 14,
+      description: "Entrenador Laparoscópico CEIQ",
+    },
+    {
+      _id: "3",
+      name: "Kit de Sutura CEIQ",
+      category: "kits",
+      image: "/images/kitSutura.jpg",
+      price: 70,
+      brand: "CEIQ",
+      rating: 4.8,
+      numReviews: 17,
+      description:
+        "equipo ideal para tus primeros pasos en la realización de sutura y nudos quirúrgicos.",
+    },
+    {
+      _id: "4",
+      name: "Simulador Diafragmático",
+      category: "endo training",
+      image: "/images/diafrag.jpg",
+      price: 15,
+      brand: "CEIQ",
+      rating: 4.6,
+      numReviews: 20,
+      description:
+        "Simula la reparación de una Hernia Hiatal Laparoscópica y abierta.",
+    },
+    {
+      _id: "5",
+      name: "CEIQ  Simulador para inyección intramuscular",
+      category: "skins",
+      image: "/images/inyIntra.jpg",
+      price: 20,
+      brand: "CEIQ",
+      rating: 4.5,
+      numReviews: 13,
+      description:
+        "simulador para la aplicación de inyecciones intramusculares simuladas.",
+    },
+    {
+      _id: "6",
+      name: "CEIQ Surgical Skin 1.0 MINI",
+      category: "skins",
+      image: "/images/skin1Mini.jpg",
+      price: 5,
+      brand: "CEIQ",
+      rating: 4.8,
+      numReviews: 25,
+      description: "Piel sintética docente en su versión pequeña",
+    },
+  ],
+};
+
+export default data;
