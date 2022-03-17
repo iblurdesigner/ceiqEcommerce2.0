500f30d53a2cd65aac4c097237a16dd49a789236
iblurdesigner <nevomen@gmail.com>
2022-03-17 16:09:14 -0500

    arreglo del css para el cart screen

--- pages/cartscreen/[qry].js
+++ pages/cartscreen/[qry].js
@@ -3,21 +3,6 @@ import { useRouter } from "next/router";
 
 export default function CartScreen({ products }) {
   const router = useRouter();
-  // const isMounted = useIsMounted();
-
-  // if (!isMounted) {
-  //   return null;
-  // }
-
-  const idPulsado = router.query._id;
-
-  //   console.log({ router }, router.query._id);
-
-  //   const product = products.find((x) => x._id === idPulsado);
-
-  // const qty = router.location.search
-  //   ? Number(router.location.search.split("=")[1])
-  //   : 1;
 
   const qty = router.asPath ? Number(router.asPath.split("=")[1]) : 1;
 
@@ -28,7 +13,7 @@ export default function CartScreen({ products }) {
   return (
     <>
       <h1>Cart Screen</h1>
-      <span>Anadir al carrito:</span>
+      <span>AÑADIR AL CARRITO:</span>
       <p>
         Producto: {qry} Cantidad: {qty}
       </p>
